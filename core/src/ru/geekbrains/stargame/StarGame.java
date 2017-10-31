5a81db008fb8e3b7a37cf2e29e4095cc9ca0724b
Shamil Aliev <inhuh005@gmail.com>
2017-10-31 18:41:13 +0300

    Initial commit

---
+++ core/src/ru/geekbrains/stargame/StarGame.java
@@ -0,0 +1,58 @@
+package ru.geekbrains.stargame;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+
+public class StarGame extends ApplicationAdapter {
+    private SpriteBatch batch;
+    private Texture img;
+    private float height;
+    private float width;
+    private boolean bool;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        img = new Texture("badlogic.jpg");
+        height = img.getHeight();
+        width = img.getWidth();
+    }
+
+    @Override
+    public void render() {
+        setBackgroundColor();
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        batch.draw(img, (Gdx.graphics.getWidth() - width) / 2,
+                (Gdx.graphics.getHeight() - height) / 2);
+        batch.end();
+    }
+
+    private void setBackgroundColor() {
+        float r = MathUtils.random(0, 1);
+        float g = MathUtils.random(0, 1);
+        float b = MathUtils.random(0, 1);
+
+        if (Gdx.input.isButtonPressed(Input.Buttons.LEFT) && bool) {
+            Gdx.gl.glClearColor(r, g, b, 1);
+            System.out.println(r + "  " + g + "   " + b);
+            bool = false;
+        }
+        if (Gdx.input.isButtonPressed(Input.Buttons.RIGHT) && !bool) {
+            Gdx.gl.glClearColor(r, g, b, 1);
+            System.out.println(r + "  " + g + "   " + b);
+            bool = true;
+        }
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        img.dispose();
+    }
+}
